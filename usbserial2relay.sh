52475bca28189e7c235abcf2178706dc141bc3e9
mazdel <delyachmad@gmail.com>
2022-11-22 17:29:15 +0700

    refactor functions, make persistent function within main script

--- usbserial2relay.sh
+++ usbserial2relay.sh
@@ -3,87 +3,128 @@
 HEX_ON='\xA0\x01\x01\xA2'
 HEX_OFF='\xA0\x01\x00\xA1'
 
-DEVICE=${1:-"/dev/ttyUSB0"}
-STATE=${2:-"1"}
-PERSISTENT=${3:-"NO"}
+DEVICE="/dev/ttyUSB0"
+STATE="ON"
+LOGFILE="$(pwd)/state.log"
 
 usage() {
-	cat <<EOF
+    cat <<EOF
 
-Usage: $0 <path_to_tty_device> <0|1>
-       0: Turn the Relay OFF
-       1: Turn the Relay ON
+Available options :
+    -d  : Device tty path. default : /dev/ttyUSB0
+    -f  : Logfile. default $(pwd)/state 
+    -h  : Show this help
+    -l  : get log of last 10 states
+    -p  : Switch relay device to the last known state.
+    -s  : Device state. default : on
+    
 
-Example: $0 /dev/ttyUSB1 0
+Usage: $0 -f ./state.log -d <path_to_tty_device> -s <on|off> 
+       
+Example: $0 -d /dev/ttyUSB1 -s on
 
 EOF
+    exit 1
 }
-sendCmd(){
-    local switch_act="$1"
+sendCmd() {
+    local device="$1"
+    local switch_act="$2"
+
     if [ "$switch_act" == "ON" ]; then
-        echo -n -e "$HEX_ON" > "$DEVICE"
+
+        echo -n -e "$HEX_ON" >"$device"
     fi
     if [ "$switch_act" == "OFF" ]; then
-        echo -n -e "$HEX_OFF" > "$DEVICE"
+
+        echo -n -e "$HEX_OFF" >"$device"
     fi
 }
-saveState(){
-    local state="$1"
-    timestamp=`date +"%FT%H-%M-%S"`
-    if [ ! -f "./state" ];then
-        touch ./state
+saveState() {
+    local device="$1"
+    local state="$2"
+
+    timestamp=$(date +"%FT%H-%M-%S")
+    if [ ! -f "${LOGFILE}" ]; then
+        touch "${LOGFILE}"
     fi
-    state_log="${timestamp}:${DEVICE}:${state}"
-    echo -e "${state_log}" >> ./state
+    state_log="${timestamp}:${device}:${state}"
+    echo -e "${state_log}" >>"${LOGFILE}"
     getLog 1
-    # if [ "${PERSISTENT}" == 'persistent' ];then
-    # 
-    # fi
-    #TODO : lanjutin ini
+
 }
-getLog(){
+getLog() {
     local lines="$1"
     local line=0
-    cat ./state | tac | while read state; do
-        stateArr=(${state//:/ })
-        
-        if [ "${stateArr[1]}" == "${DEVICE}" ];then
-            line=$(($line+1))
-            echo ${state};
+
+    if [ ! -f "${LOGFILE}" ]; then
+        touch "${LOGFILE}"
+        exit 1
+    fi
+    cat "${LOGFILE}" | tac | while read -r state; do
+        # stateArr=(${state//:/ })
+        IFS=':' read -r -a stateArr <<<"${state}"
+
+        if [ "${stateArr[1]}" == "${DEVICE}" ]; then
+            line=$(($line + 1))
+            echo "${state}"
         fi
-        if [ "$line" == "$lines" ];then
+        if [ "$line" == "$lines" ]; then
             break
-        fi        
-    done;
+        fi
+    done
 }
-switch(){
-    case "$STATE" in
-        0|[Oo][Ff][Ff])
-            sendCmd "OFF"
-            saveState "OFF"
-            
-            ;;
-        1|[Oo][Nn])
-            sendCmd "ON"
-            saveState "ON"
-            
-            ;;
-        *)
-            usage
-            exit 1
-            ;;
+switch() {
+    local device="$1"
+    local state="$2"
+    local savingState=${3:-"yes"}
+
+    if [ ! -c "$device" ]; then
+        echo -e "\ndevice $device (tty) is not exist!"
+        usage
+        exit 1
+    fi
+
+    case "$state" in
+    0 | [Oo][Ff][Ff])
+
+        sendCmd "$device" "OFF"
+        if [ "$savingState" == "yes" ]; then
+            saveState "$device" "OFF"
+        fi
+        ;;
+    1 | [Oo][Nn])
+
+        sendCmd "$device" "ON"
+        if [ "$savingState" == "yes" ]; then
+            saveState "$device" "ON"
+        fi
+        ;;
+    *)
+        echo -e "\nwrong switch state"
+        usage
+        exit 1
+        ;;
     esac
 }
-main(){
-    if [ "$DEVICE" == "-h" ];then
-        usage
-        exit 1
-    fi
-    if [ ! -c "$DEVICE" ];then
-        echo "device (tty) is not exist"
-        usage
-        exit 1
-    fi
-    switch
+persistent() {
+    local -r log=$(getLog 1)
+
+    IFS=':' read -r -a state <<<"${log}"
+    switch "$DEVICE" "${state[2]}" no
+    exit 1
 }
-main
+main() {
+    while getopts hd:s:f:pl flag; do
+        case "${flag}" in
+        d) DEVICE=${OPTARG} ;;
+        f) LOGFILE=${OPTARG} ;;
+        h) usage ;;
+        l) getLog 10 && exit 1 ;; #TODO <-- make getLog lines more flexible
+        p) persistent ;;
+        s) STATE=${OPTARG} ;;
+        *) usage ;;
+        esac
+    done
+    switch "$DEVICE" "$STATE"
+}
+main "$@"
